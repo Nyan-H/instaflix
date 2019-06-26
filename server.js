ef4b40a935cd05aa137e42985e2164809a9dcdb9
nyanxhtet <nyanhtet@yahoo.com>
2019-06-26 08:16:36 -0500

    Re-indended code

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
+fs = require('fs'),
+url = require('url');
 
-  http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
-  }).listen(8080);
-  
-  const url = require('url');
-  var addr = 'http://localhost:8080/default.htm?year=2017&month=february';
 http.createServer((request, response) => {
   var addr = request.url
   var q = url.parse(addr, true);
-
+  filePath = '';
+  
   if (q.pathname.includes('documentation')){
     filePath = (__dirname+ '/documentation.html');
   } else{
     filePath = 'index.html';
   }
-
+  
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
     if (err) {
       console.log(err);
     }
-      else {
-    console.log('Added to log.');
+    else {
+      console.log('Added to log.');
     }
   });
+}).listen(8080);
 
 
