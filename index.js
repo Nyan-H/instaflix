5e3ca34b0bafb31fc82c6cc7114efeabc836fb71
nyanxhtet <nyanhtet@yahoo.com>
2019-07-02 10:28:25 -0500

    Added Morgan library to log all requests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
+    morgan = require('morgan');
 const app  = express();
+app.use(morgan('common'));
 //top ten movies
 let topMovies = [ {
     title: 'Inception'
