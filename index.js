f0b6d47964ff1d07b9fb5d52e74dfe1a5841fccd
nyanxhtet <nyanhtet@yahoo.com>
2019-07-02 10:23:25 -0500

    added a top ten movies json object

---
+++ index.js
@@ -0,0 +1,33 @@
+//top ten movies
+let topMovies = [ {
+    title: 'Inception'
+},
+{
+    title: 'Jusrasic Park'
+},
+{
+    title: 'Shutter Island'
+},
+{
+    title: 'Pulp Fiction'
+},
+{
+    title: 'Elf'
+},
+{
+    title: 'A Quiet Place'
+},
+{
+    title: 'Black Panther'
+},
+{
+    title: 'Us'
+},
+{
+    title: 'Logan'
+},
+{
+    title: 'Get Out'
+},
+
+]
