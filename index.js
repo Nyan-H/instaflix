cfc460b4049f9afa7cef5c9e8825b8207586424e
nyanxhtet <nyanhtet@yahoo.com>
2019-07-03 09:14:10 -0500

    Reindented with 2-spaces per indentation to maintain consistent.

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const express = require('express'),
-    morgan = require('morgan');
+morgan = require('morgan');
 
 const app  = express();
 
 //top ten movies
 let topMovies = [ {
-    title: 'Inception'
+  title: 'Inception'
 },
 {
-    title: 'Jusrasic Park'
+  title: 'Jusrasic Park'
 },
 {
-    title: 'Shutter Island'
+  title: 'Shutter Island'
 },
 {
-    title: 'Pulp Fiction'
+  title: 'Pulp Fiction'
 },
 {
-    title: 'Elf'
+  title: 'Elf'
 },
 {
-    title: 'A Quiet Place'
+  title: 'A Quiet Place'
 },
 {
-    title: 'Black Panther'
+  title: 'Black Panther'
 },
 {
-    title: 'Us'
+  title: 'Us'
 },
 {
-    title: 'Logan'
+  title: 'Logan'
 },
 {
-    title: 'Get Out'
+  title: 'Get Out'
 },
 
 ]
@@ -43,18 +43,18 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 app.get('/movies', function(req, res) {
-    res.json(topMovies)
+  res.json(topMovies)
 });
 
 app.get('/', function(req, res) {
-    res.send('Welcome to Instaflix, Instant Flix on demand!')
+  res.send('Welcome to Instaflix, Instant Flix on demand!')
 });
 
 app.listen(8080, () =>
-    console.log('App is listening on port 8080')
+console.log('App is listening on port 8080')
 );
 
 app.use(function (err, req, res, next){
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
