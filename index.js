f6dc1b61ca114b791989eb9666057b72c55b48b4
Phonetic Converter <nyanhtet@yahoo.com>
2020-01-17 13:04:16 -0800

    made changes to the directory

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function vowelsOnly(str){
+    for (i=0; i<(str).length(); i++){
+       if (str.match(/[aeiouAEIOU]/gi)){
+           return i;
+
+        for (i= 0;i < str.length(); i++) {
+        (str) += s.charAt(i);
+    }
+       }
+       }
+   }
--- instaflix/app/server.js
+++ instaflix/app/server.js
@@ -4,7 +4,7 @@ morgan = require('morgan');
 const app  = express();
 
 //top ten movies
-let movies = [ {
+let topMovies = [ {
   title: 'Inception'
 },
 {
@@ -43,42 +43,18 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 app.get('/movies', function(req, res) {
-  res.json(movies)
+  res.json(topMovies)
 });
 
 app.get('/', function(req, res) {
   res.send('Welcome to Instaflix, Instant Flix on demand!')
 });
 
-app.get('/documentation', function(req, res) {
-  res.sendFile('./public/documentation.html', { root: __dirname});
-});
-
-
 app.get('/movies/:name', (req, res) => {
   res.json(Students.find( (student) =>
     { return student.name === req.params.name   }));
 });
-
-app.post('/movies', (req,res) => {
-  let newMovie = req.body;
-
-  if(!newMovie.name) {
-    const message = 'missing "name" in request body';
-    res.status(400).send.apply(message);
-  } else {
-    newMovie.id = uuid.v4();
-    topMovies.push(newMovie);
-    res.status(201).send(newMovie);
-  }
-
-
-});
-
-
-
-
-
+ 
 
 
 
@@ -90,3 +66,5 @@ app.use(function (err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+
