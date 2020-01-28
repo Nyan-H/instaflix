686756ea30f7682a0412163cce3459bb678d3254
Phonetic Converter <nyanhtet@yahoo.com>
2020-01-27 22:38:30 -0800

    created a "User" and "Movie" Schemas for MongoDB database

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+
+var movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description : {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+
+    Director: {
+        Name: String,
+        Bio: String
+    },
+
+    Actors: [String],
+    ImagePath : String,
+    Featured: Boolean
+
+});
+
+var userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, requrird: true},
+    Email: {type: String, requried: true},
+    Birthday : Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+
+});
+
+var Movie = mongoose.model('Movie', movieSchema);
+var User = mongoose.model('User', userSchema);
