a1748acd469308eb1f550607646422428ef43482
Phonetic Converter <nyanhtet@yahoo.com>
2020-01-29 12:24:56 -0800

    edited models.js name typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 morgan = require('morgan');
 const mongoose = require('mongoose');
-const Models = require('./models.j');
+const Models = require('./models.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
