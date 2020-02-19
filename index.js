07f8ed85563b73de8a57404ba1ab8a409e354cea
Phonetic Converter <nyanhtet@yahoo.com>
2020-02-18 23:00:51 -0800

    Added a delete request to delete users by usernames

--- index.js
+++ index.js
@@ -121,6 +121,36 @@ app.put('/users/:Username', function(req, res) {
   })
 });
 
+// Delete a user by username
+app.delete('/users/:Username', function(req, res) {
+  Users.findOneAndRemove({ Username: req.params.Username })
+  .then(function(user) {
+    if (!user) {
+      res.status(400).send(req.params.Username + " was not found");
+    } else {
+      res.status(200).send(req.params.Username + " was deleted.");
+    }
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(8080, () =>
