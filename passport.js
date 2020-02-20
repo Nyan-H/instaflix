fa2d174121bbe0e7b9703f60b1e047bf43ca9d40
Phonetic Converter <nyanhtet@yahoo.com>
2020-02-19 21:57:21 -0800

    added a local strategy

--- passport.js
+++ passport.js
@@ -3,3 +3,40 @@ const passport = require('passport'),
   Models = require('./models.js'),
   passportJWT= require('passport-jwt');
 
+var Users = Models.User;
+var JWTStrategy = passportJWT.Strategy;
+var ExtractJWT = passportJWT.ExtractJwt;
+
+
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passportField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + ' ' + password);
+  Users.findOne({ Username: username}, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+    if (!user) {
+      console.log('incorret username');
+      return callback(null, false, {message:'Incorrect username or password.'});
+    }
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
+
+passport.use( new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secret0rKey: 'instaflix-key'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+  .then((user) => {
+    return callback(null, user);
+  })
+  .catch((error) => {
+    return callback(error)
+  });
+}));
+
